0c851c540779d341b92f82cd1a6f96f789661da7
teme-qualia <82814784+teme-qualia@users.noreply.github.com>
2023-12-22 14:15:38 +0900

    ログイン側の機能を作成

--- django-multiple-user-types-example/django_school/classroom/views/classroom.py
+++ django-multiple-user-types-example/django_school/classroom/views/classroom.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render
 from django.views.generic import TemplateView
+from django.contrib.auth import authenticate, login
 
 
 class SignUpView(TemplateView):
@@ -7,9 +8,29 @@ class SignUpView(TemplateView):
 
 
 def home(request):
+    context = {}
+    if request.method == 'POST':
+        user = _login(request)
+        if user is None:
+            context['error_message'] = 'ユーザーIDまたは、パスワードが誤っています。'
+
     if request.user.is_authenticated:
         if request.user.is_teacher:
             return redirect('teachers:quiz_change_list')
         else:
             return redirect('students:quiz_list')
-    return render(request, 'classroom/home.html')
+
+    return render(request, 'classroom/home.html', context)
+
+
+def _login(request):
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        # Redirect to a success page.
+        return user
+    else:
+        # Return an 'invalid login' error message.
+        ...
